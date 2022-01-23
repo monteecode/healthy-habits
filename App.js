19d4589cef5cf6bc9dc295f86b650f8b7f732622
Montensia Banks <montensia@gmail.com>
2022-01-23 17:35:03 -0600

    Added styles to list and displayed entered goal

--- App.js
+++ App.js
@@ -10,29 +10,16 @@ export default function App() {
   };
   const addGoalHandler = () => {
     // setenteredGoal([...healthGoals, enteredGoal]);
-    setenteredGoal((currentGoals) => [...currentGoals, enteredGoal]);
+    sethealthGoals((currentGoals) => [...currentGoals, enteredGoal]);
   };
   return (
-    <View
-      style={{
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <View style={{ backgroundColor: "red", width: "100%" }}>
         <TextInput
           placeholder="Enter health goal"
           onChangeText={goalInputHandler}
           value={enteredGoal}
-          style={{
-            marginBottom: 10,
-
-            borderBottomColor: "black",
-            borderBottomWidth: 1,
-            padding: 10,
-          }}
+          style={styles.inputContainer}
         />
         <Button
           title="ADD"
@@ -40,16 +27,38 @@ export default function App() {
           onPress={addGoalHandler}
         />
       </View>
-      <View></View>
+      <View style={styles.list}>
+        {healthGoals.map((goal, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text>{`${index + 1}. ${goal}`}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    padding: 50,
+    backgroundColor: "#fff",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginBottom: 10,
+
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  list: {
+    backgroundColor: "blue",
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: "#ccc",
+  },
+});
