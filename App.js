2a37c7c4ffde38deffb62c336681030084662884
Montensia Banks <montensia@gmail.com>
2022-01-23 16:56:35 -0600

    Added state and inital styling

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setenteredGoal] = useState("");
+  const [healthGoals, sethealthGoals] = useState([]);
+  const goalInputHandler = (enteredText) => {
+    setenteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    // setenteredGoal([...healthGoals, enteredGoal]);
+    setenteredGoal((currentGoals) => [...currentGoals, enteredGoal]);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <View style={{ backgroundColor: "red", width: "100%" }}>
+        <TextInput
+          placeholder="Enter health goal"
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+          style={{
+            marginBottom: 10,
+
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button
+          title="ADD"
+          style={{ marginBottom: 20 }}
+          onPress={addGoalHandler}
+        />
+      </View>
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
